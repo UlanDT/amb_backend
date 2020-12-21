f762231d8511bed5b5cd326af35acebad19d78c8
Ulandt <ulanbek.dt94@gmail.com>
2020-12-21 12:46:29 +0600

    added notification feature

--- sox_backend/settings.py
+++ sox_backend/settings.py
@@ -124,3 +124,9 @@ STATICFILES_DIRS = [
 ]
 
 CART_SESSION_ID = 'cart'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ulanbek.dt94@gmail.com'
+EMAIL_HOST_PASSWORD = 'PythonProgramming2020'
