7b7f5c53b64e3d000e32bb92110e6a25da8ea4ed
Ulandt <ulanbek.dt94@gmail.com>
2020-12-21 13:38:07 +0600

    privacy fix

--- sox_backend/settings.py
+++ sox_backend/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,10 +130,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '<EMAIL_HOST_USER>'
-EMAIL_HOST_PASSWORD = '<EMAIL_HOST_PASSWORD>'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
-BRAINTREE_MERCHANT_ID = '<BRAINTREE_MERCHANT_ID>'
-BRAINTREE_PUBLIC_KEY = '<BRAINTREE_PUBLIC_KEY>'
-BRAINTREE_PRIVATE_KEY = '<BRAINTREE_PRIVATE_KEY>'
+BRAINTREE_MERCHANT_ID = os.getenv("BRAINTREE_MERCHANT_ID")
+BRAINTREE_PUBLIC_KEY = os.getenv("BRAINTREE_PUBLIC_KEY")
+BRAINTREE_PRIVATE_KEY = os.getenv("BRAINTREE_PRIVATE_KEY")
 
