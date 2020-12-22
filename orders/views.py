90fb2ba6f175fe0c7efba527ecec0f3a5251e38c
Ulandt <ulanbek.dt94@gmail.com>
2020-12-22 15:59:43 +0600

    admin restructure

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
@@ -15,9 +16,9 @@ def order_create(request):
             order = form.save()
             for item in cart:
                 OrderItem.objects.create(order=order,
-                                        product=item['product'],
-                                        price=item['price'],
-                                        quantity=item['quantity'])
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
             # clear the cart
             cart.clear()
             order_created.delay(order.id)
@@ -29,3 +30,9 @@ def order_create(request):
     return render(request,
                   'orders/create.html',
                   {'cart': cart, 'form': form})
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request, 'orders/admin.html', {'order': order})
